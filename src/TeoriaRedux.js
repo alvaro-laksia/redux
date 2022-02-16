420ee5f336d3fe8ec93db370ac79bad8752a125b
alvaro-78 <alvaro78an@gmail.com>
2022-02-16 08:45:21 +0100

    Removed points from list tag

--- src/TeoriaRedux.js
+++ src/TeoriaRedux.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './TeoriaRedux.css';
+
 import reduxGif from './assets/redux.gif';
 import reduxImg from './assets/redux.jpg';
 
@@ -8,32 +10,32 @@ const TeoriaRedux = () => {
 		<div>
 			<h2>Teoría y Conceptos</h2>
 			<ul>
-				<li>
+				<li className="list">
 					<b>
 						<i>Store</i>
 					</b>
 					, contiene el <i>state</i>, (uno por aplicación).
 				</li>
-				<li>
+				<li className="list">
 					<b>
 						<i>Dispatch</i>
 					</b>
 					, ejecuta una acción que actualizará el <i>state</i>.
 				</li>
-				<li>
+				<li className="list">
 					<b>
 						<i>Action</i>
 					</b>
 					, objeto <i>JS</i> con 2 propiedades: <i>type</i> y <i>payload</i>{' '}
 					(datos).
 				</li>
-				<li>
+				<li className="list">
 					<b>
 						<i>Subscribe</i>
 					</b>
 					, es un manejador de eventos para el <i>state</i>.
 				</li>
-				<li>
+				<li className="list">
 					<b>
 						<i>Reducers</i>
 					</b>
