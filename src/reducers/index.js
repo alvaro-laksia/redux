ef6f0cf9ea64b80f560edd6101d55e35ed69814e
alvaro-78 <alvaro78an@gmail.com>
2022-02-17 08:32:29 +0100

    Added shoppingCart compoennt

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import counterReducer from './counterReducer';
+import { shoppingReducer } from './shoppingReducer';
+import { crudReducer } from './crudReducer';
 
 const reducer = combineReducers({
 	counter: counterReducer,
+	shopping: shoppingReducer,
+	crud: crudReducer,
 });
 
 export default reducer;
