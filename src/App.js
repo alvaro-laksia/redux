678908f70e54623a0e0e4e1e01e1e9c8956851fa
alvaro-78 <alvaro78an@gmail.com>
2022-02-17 10:40:50 +0100

    Added CrudApi to render in App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import { ReduxTheory } from './components/reduxTheory/ReduxTheory';
 import { Counter } from './components/counter/Counter.jsx';
-import ShoppingCart from './components/ShoppingCart';
+import { ShoppingCart } from './components/ShoppingCart';
+import { CrudApi } from './components/CrudApi';
 
 function App() {
 	return (
 		<div className="App">
+			<CrudApi />
+			<hr />
 			<Counter />
 			<hr />
 			<ShoppingCart />
