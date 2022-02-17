64f5f774ce44e019cc130afdbd875f0efd69e14f
alvaro-78 <alvaro78an@gmail.com>
2022-02-17 10:40:25 +0100

    Added CrudApi component

--- src/components/CrudApi.js
+++ src/components/CrudApi.js
@@ -1,136 +1,142 @@
-import React, { useEffect, useReducer, useState } from "react";
-import { TYPES } from "../actions/crudActions";
-import { helpHttp } from "../helpers/helpHttp";
-import { crudInitialState, crudReducer } from "../reducers/crudReducer";
-import CrudForm from "./CrudForm";
-import CrudTable from "./CrudTable";
-import Loader from "./Loader";
-import Message from "./Message";
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { helpHttp } from '../helpers/helpHttp';
+import {
+	createAction,
+	noAction,
+	readAllAction,
+	deleteAction,
+	updateAction,
+} from '../actions/crudActions';
+import CrudForm from './CrudForm';
+import CrudTable from './CrudTable';
+import { Loader } from './loader/Loader';
+import Message from './Message';
 
-const CrudApi = () => {
-  //const [db, setDb] = useState(null);
-  const [state, dispatch] = useReducer(crudReducer, crudInitialState);
-  const { db } = state;
-  const [dataToEdit, setDataToEdit] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+export const CrudApi = () => {
+	//const [db, setDb] = useState(null);
+	const state = useSelector((state) => state);
+	const dispatch = useDispatch();
+	const { db } = state.crud;
 
-  let api = helpHttp();
-  let url = "http://localhost:5000/santos";
+	const [dataToEdit, setDataToEdit] = useState(null);
+	const [error, setError] = useState(null);
+	const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    setLoading(true);
-    helpHttp()
-      .get(url)
-      .then((res) => {
-        //console.log(res);
-        if (!res.err) {
-          //setDb(res);
-          dispatch({ type: TYPES.READ_ALL_DATA, payload: res });
-          setError(null);
-        } else {
-          //setDb(null);
-          dispatch({ type: TYPES.NO_DATA });
-          setError(res);
-        }
-        setLoading(false);
-      });
-  }, [url]);
+	let api = helpHttp();
+	let url = 'http://localhost:5000/santos';
 
-  const createData = (data) => {
-    data.id = Date.now();
-    //console.log(data);
+	useEffect(() => {
+		setLoading(true);
+		helpHttp()
+			.get(url)
+			.then((res) => {
+				//console.log(res);
+				if (!res.err) {
+					//setDb(res);
+					dispatch(readAllAction(res));
+					setError(null);
+				} else {
+					//setDb(null);
+					dispatch(noAction());
+					setError(res);
+				}
+				setLoading(false);
+			});
+	}, [url, dispatch]);
 
-    let options = {
-      body: data,
-      headers: { "content-type": "application/json" },
-    };
+	const createData = (data) => {
+		data.id = Date.now();
+		//console.log(data);
 
-    api.post(url, options).then((res) => {
-      //console.log(res);
-      if (!res.err) {
-        //setDb([...db, res]);
-        dispatch({ type: TYPES.CREATE_DATA, payload: res });
-      } else {
-        setError(res);
-      }
-    });
-  };
+		let options = {
+			body: data,
+			headers: { 'content-type': 'application/json' },
+		};
 
-  const updateData = (data) => {
-    let endpoint = `${url}/${data.id}`;
-    //console.log(endpoint);
+		api.post(url, options).then((res) => {
+			//console.log(res);
+			if (!res.err) {
+				//setDb([...db, res]);
+				dispatch(dispatch(createAction(res)));
+			} else {
+				setError(res);
+			}
+		});
+	};
 
-    let options = {
-      body: data,
-      headers: { "content-type": "application/json" },
-    };
+	const updateData = (data) => {
+		let endpoint = `${url}/${data.id}`;
+		//console.log(endpoint);
 
-    api.put(endpoint, options).then((res) => {
-      //console.log(res);
-      if (!res.err) {
-        //let newData = db.map((el) => (el.id === data.id ? data : el));
-        //setDb(newData)
-        dispatch({ type: TYPES.UPDATE_DATA, payload: data });
-      } else {
-        setError(res);
-      }
-    });
-  };
+		let options = {
+			body: data,
+			headers: { 'content-type': 'application/json' },
+		};
 
-  const deleteData = (id) => {
-    let isDelete = window.confirm(
-      `¿Estás seguro de eliminar el registro con el id '${id}'?`
-    );
+		api.put(endpoint, options).then((res) => {
+			//console.log(res);
+			if (!res.err) {
+				//let newData = db.map((el) => (el.id === data.id ? data : el));
+				//setDb(newData)
+				dispatch(updateAction(res));
+			} else {
+				setError(res);
+			}
+		});
+	};
 
-    if (isDelete) {
-      let endpoint = `${url}/${id}`;
-      let options = {
-        headers: { "content-type": "application/json" },
-      };
+	const deleteData = (id) => {
+		let isDelete = window.confirm(
+			`¿Estás seguro de eliminar el registro con el id '${id}'?`
+		);
 
-      api.del(endpoint, options).then((res) => {
-        //console.log(res);
-        if (!res.err) {
-          //let newData = db.filter((el) => el.id !== id);
-          //setDb(newData);
-          dispatch({ type: TYPES.DELETE_DATA, payload: id });
-        } else {
-          setError(res);
-        }
-      });
-    } else {
-      return;
-    }
-  };
+		if (isDelete) {
+			let endpoint = `${url}/${id}`;
+			let options = {
+				headers: { 'content-type': 'application/json' },
+			};
 
-  return (
-    <div>
-      <h2>CRUD API</h2>
-      <article className="grid-1-2">
-        <CrudForm
-          createData={createData}
-          updateData={updateData}
-          dataToEdit={dataToEdit}
-          setDataToEdit={setDataToEdit}
-        />
-        {loading && <Loader />}
-        {error && (
-          <Message
-            msg={`Error ${error.status}: ${error.statusText}`}
-            bgColor="#dc3545"
-          />
-        )}
-        {db && (
-          <CrudTable
-            data={db}
-            setDataToEdit={setDataToEdit}
-            deleteData={deleteData}
-          />
-        )}
-      </article>
-    </div>
-  );
+			api.del(endpoint, options).then((res) => {
+				//console.log(res);
+				if (!res.err) {
+					//let newData = db.filter((el) => el.id !== id);
+					//setDb(newData);
+					dispatch(deleteAction(id));
+				} else {
+					setError(res);
+				}
+			});
+		} else {
+			return;
+		}
+	};
+
+	return (
+		<div>
+			<h2>CRUD API</h2>
+			<article className="grid-1-2">
+				<CrudForm
+					createData={createData}
+					updateData={updateData}
+					dataToEdit={dataToEdit}
+					setDataToEdit={setDataToEdit}
+				/>
+				{loading && <Loader />}
+				{error && (
+					<Message
+						msg={`Error ${error.status}: ${error.statusText}`}
+						bgColor="#dc3545"
+					/>
+				)}
+				{db && (
+					<CrudTable
+						data={db}
+						setDataToEdit={setDataToEdit}
+						deleteData={deleteData}
+					/>
+				)}
+			</article>
+		</div>
+	);
 };
-
-export default CrudApi;
