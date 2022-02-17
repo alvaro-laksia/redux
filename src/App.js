4f6f811c583769283141bb9d3834882be2334c5b
alvaro-78 <alvaro78an@gmail.com>
2022-02-17 08:33:19 +0100

    Added Message component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import { ReduxTheory } from './components/reduxTheory/ReduxTheory';
 import { Counter } from './components/counter/Counter.jsx';
+import ShoppingCart from './components/ShoppingCart';
 
 function App() {
 	return (
 		<div className="App">
 			<Counter />
+			<hr />
+			<ShoppingCart />
 			<hr />
 			<ReduxTheory />
 		</div>
